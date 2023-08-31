d1a6db1608e56e0b0c5788329c35bf724c5be4aa
prathamrehil <prathamrehil@gmail.com>
2023-08-31 18:33:45 +0530

    6

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from dataclasses import dataclass
 
 
+@dataclass
 class Result:
     result: int
 
@@ -14,10 +15,12 @@ def greeting():
     return 'Hello world!'
 
 
-@app.route("/calculator/add", methods=['POST'])
+app.route("/calculator/add", methods=['POST'])
+
+
 def add():
     numbers = request.json
-    response = Result(numbers['first']+numbers['second'])
+    response = Result(numbers['first'] + numbers['second'])
     return jsonify(response)
 
 
@@ -29,4 +32,4 @@ def subtract():
 
 
 if __name__ == '__main__':
-    app.run(port=8000, host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0')
