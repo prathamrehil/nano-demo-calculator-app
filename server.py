a50c7938f6b1f9df2e2f8e95ce8204c157618706
prathamrehil <prathamrehil@gmail.com>
2023-08-31 18:01:34 +0530

    2

--- server.py
+++ server.py
@@ -1,36 +1,32 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from dataclasses import dataclass
+
+
+class Result:
+    result: int
+
 
 app = Flask(__name__)
 
+
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return 'Hello World!'
+    return 'Hello world!'
+
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    data = request.get_json()  # Assuming JSON data in the POST request
-    first = data.get('first')
-    second = data.get('second')
-    if first is None or second is None:
-        return "Missing 'first' or 'second' in the request data", 400
-    try:
-        result = float(first) + float(second)
-        return str(result)
-    except ValueError:
-        return "Invalid input data, 'first' and 'second' should be numbers", 400
+    numbers = request.json
+    response = Result(numbers['first'+numbers['second']])
+    return jsonify(response)
+
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    data = request.get_json()  # Assuming JSON data in the POST request
-    first = data.get('first')
-    second = data.get('second')
-    if first is None or second is None:
-        return "Missing 'first' or 'second' in the request data", 400
-    try:
-        result = float(first) - float(second)
-        return str(result)
-    except ValueError:
-        return "Invalid input data, 'first' and 'second' should be numbers", 400
+    numbers = request.json
+    response = Result(numbers['first'] - numbers['second'])
+    return jsonify(response)
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='0.0.0.0')
